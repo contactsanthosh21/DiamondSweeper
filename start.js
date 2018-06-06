58b22e99ea255cfbd5185f54351f383948d4513e
santhosh <santhosh@tataelxsi.co.in>
2018-06-06 16:58:44 +0530

    Diamond sweeper Assignment part 2 with hints

--- start.js
+++ start.js
@@ -1,13 +1,3 @@
-/*
- * File Name: start.js
- * Description: Module handles the app running.
- *----------*
- * node  |  * licensed under the MIT license. | https://raw.githubusercontent.com/nodejs/node/master/LICENSE
- *----------*
- * Copyright (c) 2018: 
- * Authors: Santhosh S
- * Creation Date: 05 June, 2018
- */
 const express = require('express');
 const app = express();
 
@@ -15,10 +5,10 @@ app.set("view engine", "ejs");
 
 app.use(express.static('public')) ;
 
-app.get('/home', function (req, res) {
+app.get('/', function (req, res) {
   res.render("home/index")
 });
 
 app.listen(3000, function () {
-  console.log('Diamond Sweeper app listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 });
